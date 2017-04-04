245ff3234042c528fe11d58a3e359af1101a9060
Sam Partridge <sampartr@gmail.com>
2017-04-04 22:34:01 +0100

    Cleanup index route

--- server/frontend/web.js
+++ server/frontend/web.js
@@ -5,7 +5,7 @@ var expressNunjucks = require('express-nunjucks');
 class WebFront {
     constructor(settings, expressApp) {
         this.app = expressApp;
-        this.app.set('view engine', 'njk')
+        this.app.set('view engine', 'njk');
         this.app.set('views', settings.web.srcDir);
         this.njk = expressNunjucks(this.app, {});
 
@@ -24,10 +24,6 @@ class WebFront {
     setupRoutes() {
         this.app.get('/', (req, res) => {this._index(req, res);});
         this.app.get('/stream', (req, res) => {this._stream(req, res);});
-    }
-
-    _index(req, res) {
-        res.render('index');
     }
 
     _stream(req, res) {
